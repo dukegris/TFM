000b75837e0528abbbbc3a28c4909110f3379839
Raúl Cuesta Saínz <dukegris@gmail.com>
2020-07-31 13:38:23 +0200

    20200731 - Contenedores base de Docker

--- es.rcs.tfm/es.rcs.tfm.spa/src/environments/environment.prod.ts
+++ es.rcs.tfm/es.rcs.tfm.spa/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+	numRowsInPage: 20,
+	accountLoginUrl: '/account/login',
+	accountMeUrl: '/account/me',
+	apiUrl: 'http://tfm_master/api',
+	crnkApiUrl: 'http://tfm_master/api/data',
+	applicationUrl: '/applications',
+	moduleUrl: '/modules',
+	functionUrl: '/functions',
 };
